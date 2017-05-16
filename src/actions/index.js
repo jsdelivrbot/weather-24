daeaf47cc9e1551b88682fab8c41979f4a03db30
Lucas Caixeta <contato@lucascaixeta.com>
2017-05-16 14:57:42 +0100

    Ajax Requests with Axios - Section 5, Lecture 57

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
-const API_KEY = '075afbccb95851152ea0b3e15c4ad006';
+import axios from 'axios';
+
+const API_KEY  = '075afbccb95851152ea0b3e15c4ad006';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url     = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type   : FETCH_WEATHER,
+    payload: request
+  };
+}
+
