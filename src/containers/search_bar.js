bbcd8bf35205912d78a62bd4d87f38098548edf5
Lucas Caixeta <contato@lucascaixeta.com>
2017-05-16 11:43:07 +0100

    Form Elements in React - Section 5, Lecture 54

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,33 +3,41 @@ import React, {Component} from 'react';
 
 export default class SearchBar extends Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {term: ''};
+    this.state = {term: ''};
 
-        this.onInputChange = this.onInputChange.bind(this);
-    }
+    //this = instance of SearchBar
+    //overwriting method
+    this.onInputChange = this.onInputChange.bind(this);
+  }
 
-    onInputChange(event) {
-        this.setState({term: event.target.value})
-    }
+  onInputChange(event) {
+    this.setState({term: event.target.value});
+  }
 
-    render() {
-        return (
-            <form className="input-group">
-                <input
-                    placeholder="Get a five-forecast in your favorite cities"
-                    className="form-control"
-                    value={this.state.term}
-                    onChange={this.onInputChange}
-                />
-                <span className="input-group-btn">
-                    <button type="submit" className="btn btn-secondary">Submit</button>
-                </span>
-            </form>
-        )
-    }
+  static onFormSubmit(event){
+    event.preventDefault();
+
+    // We need to go and fetch weather data
+  }
+
+  render() {
+    return (
+      <form onSubmit={SearchBar.onFormSubmit} className="input-group">
+        <input
+          placeholder="Get a five-forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+          <span className="input-group-btn">
+              <button type="submit" className="btn btn-secondary">Submit</button>
+          </span>
+      </form>
+    )
+  }
 }
 
 
