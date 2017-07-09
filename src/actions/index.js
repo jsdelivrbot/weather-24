34e6740cb4ff7178b9f706a77a68d809f2a1bf4c
Lucas Caixeta <contato@lucascaixeta.com>
2017-07-09 15:41:47 +0100

    Redux-Promisse in Action - Section 5, Lecture 58

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY  = '075afbccb95851152ea0b3e15c4ad006';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
